bdfdf52481c85723ddc5503c8152d8254e4ac096
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-03-02 21:25:55 +0900

    天中殺を表示

--- app/models/sexagenary_cycle.rb
+++ app/models/sexagenary_cycle.rb
@@ -5,6 +5,15 @@ class SexagenaryCycle < ApplicationRecord
   has_many :fortune_analysis_months, class_name: "FortuneAnalysis", foreign_key: "sexagenary_cycle_month_id"
   has_many :fortune_analysis_days, class_name: "FortuneAnalysis", foreign_key: "sexagenary_cycle_day_id"
 
+  enum :heavenly_void, {
+    '戊亥天中殺': 0,
+    '申酉天中殺': 1,
+    '午未天中殺': 2,
+    '辰巳天中殺': 3,
+    '寅卯天中殺': 4,
+    '子丑天中殺': 5
+  }
+
   def stem_and_branch
     "#{stem.name}#{branch.name}"
   end
