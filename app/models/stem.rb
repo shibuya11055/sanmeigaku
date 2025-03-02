11a12e554b8e81e34f9b2d388340d17974b02e3b
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-03-02 23:43:48 +0900

    宿命絵図を追加

--- app/models/stem.rb
+++ app/models/stem.rb
@@ -6,4 +6,36 @@ class Stem < ApplicationRecord
   has_many :stem_ten_star_mappings_as_main_stem, class_name: 'StemTenStarMapping', foreign_key: 'main_stem_id'
   has_many :stem_ten_star_mappings_as_sub_stem, class_name: 'StemTenStarMapping', foreign_key: 'sub_stem_id'
   has_many :stem_twelve_star_mappings, class_name: 'StemTwelveStarMapping'
+
+  enum :yin_yang, {
+    '陰': 0,
+    '陽': 1
+  }
+
+  def image_name
+    case name
+    when '甲'
+      '甲木陽.png'
+    when '乙'
+      '乙木陰.png'
+    when '丙'
+      '丙火陽.png'
+    when '丁'
+      '丁火陰.png'
+    when '戊'
+      '戊土陽.png'
+    when '己'
+      '己土陰.png'
+    when '庚'
+      '庚金陽.png'
+    when '辛'
+      '辛金陰.png'
+    when '壬'
+      '壬水陽.png'
+    when '癸'
+      '癸水陰.png'
+    else
+      nil
+    end
+  end
 end
--- app/models/ten_major_star.rb
+++ app/models/ten_major_star.rb
@@ -3,4 +3,9 @@ class TenMajorStar < ApplicationRecord
   has_many :stem_ten_star_mappings
   has_many :main_stems, through: :stem_ten_star_mappings, source: :main_stem
   has_many :sub_stems, through: :stem_ten_star_mappings, source: :sub_stem
+
+  enum :yin_yang, {
+    '陰': 0,
+    '陽': 1
+  }
 end
