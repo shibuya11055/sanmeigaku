4aaf07cd8e0ec419be65fd1d90578470a17e1939
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-03-07 00:33:30 +0900

    ようせんの背景画像、標準の気の流れを表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def css_classes(conditions)
     conditions.select { |_class, condition| condition }.keys.join(' ')
   end
+
+  def bg_img_class(value)
+    return '' unless value
+    "bg-img-#{value.element_id}-#{value.yin_yang_before_type_cast}"
+  end
 end
--- app/models/ten_major_star.rb
+++ app/models/ten_major_star.rb
@@ -8,4 +8,31 @@ class TenMajorStar < ApplicationRecord
     '陰': 0,
     '陽': 1
   }
+
+  def image_name
+    case name
+    when '貫索星'
+      '甲木陽.png'
+    when '石門星'
+      '乙木陰.png'
+    when '鳳閣星'
+      '丙火陽.png'
+    when '調舒星'
+      '丁火陰.png'
+    when '禄存星'
+      '戊土陽.png'
+    when '司禄星'
+      '己土陰.png'
+    when '車騎星'
+      '庚金陽.png'
+    when '牽牛星'
+      '辛金陰.png'
+    when '龍高星'
+      '壬水陽.png'
+    when '玉堂星'
+      '癸水陰.png'
+    else
+      nil
+    end
+  end
 end
