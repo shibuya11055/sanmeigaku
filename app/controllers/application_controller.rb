78c1b6d39c2ac1501cf599e3d9fc5aba51a56be0
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-03-15 02:51:44 +0900

    basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :basic_auth, if: -> { Rails.env.production? }
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
