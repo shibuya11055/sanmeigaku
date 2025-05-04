ad4ab99818aba6b1129c9f6b9fcb7caf1c428b1c
kyohei.shibuya <shibuya01055@gmail.com>
2025-05-04 21:53:55 +0900

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:         "sanmeigaku.app@gmail.com",
-    port:            587,
-    domain:          "sanmeigaku.app@gmail.com",
-    user_name:       Rails.application.credentials.dig(:smtp, :user_name),
-    password:        Rails.application.credentials.dig(:smtp, :password),
-    authentication:  "login",
-    enable_starttls: true,
-    open_timeout:    5,
-    read_timeout:    5 }
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            Rails.application.credentials.dig(:smtp, :user_name),
+    password:             Rails.application.credentials.dig(:smtp, :password),
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    open_timeout:          5,
+    read_timeout:          5
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
